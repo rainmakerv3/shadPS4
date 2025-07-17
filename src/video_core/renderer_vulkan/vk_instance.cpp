25739eefaca8cdece3a16e54cf4b81323f9af045
rainmakerv2 <30595646+rainmakerv3@users.noreply.github.com>
2025-07-18 06:02:28 +0800

    new 5_6_5 commit

--- src/video_core/renderer_vulkan/vk_instance.cpp
+++ src/video_core/renderer_vulkan/vk_instance.cpp
@@ -675,11 +675,6 @@ vk::Format Instance::GetSupportedFormat(const vk::Format format,
                 return vk::Format::eR8G8B8A8Srgb;
             }
             break;
-        case vk::Format::eB5G6R5UnormPack16:
-            if (IsFormatSupported(vk::Format::eB8G8R8A8Unorm, flags)) {
-                return vk::Format::eB8G8R8A8Unorm;
-            }
-            break;
         default:
             break;
         }
